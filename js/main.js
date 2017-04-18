1e5ef6116944a9622481a4133e9037941aca119f
Kelly Marton <kellymmarton@gmail.com>
2017-04-17 23:12:47 -0400

    add slimMenu files and tags

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('#navigation').slimmenu(
+        {
+            resizeWidth: '800',
+            collapserTitle: 'Site Menu',
+            animSpeed: 'medium',
+            easingEffect: null,
+            indentChildren: false,
+            childrenIndenter: '&nbsp;'
+        });
+});
