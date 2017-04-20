6ca2d0f8497daa0f47eff4d89abe3bedc268b97d
Kelly Marton <kellymmarton@gmail.com>
2017-04-19 22:39:04 -0400

    add custom jQuery to change menu item color

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
 
-    $('#navigation').slimmenu(
-        {
-            resizeWidth: '800',
-            collapserTitle: 'Site Menu',
-            animSpeed: 'medium',
-            easingEffect: null,
-            indentChildren: false,
-            childrenIndenter: '&nbsp;'
-        });
+    $('#navigation').slimmenu({
+        resizeWidth: '800',
+        collapserTitle: 'Site Menu',
+        animSpeed: 'medium',
+        easingEffect: null,
+        indentChildren: false,
+        childrenIndenter: '&nbsp;'
+    });
+
+    $('ul li').mouseover(function () {
+        $('li').css('color', '#f11c24');
+    });
+    $('ul li').mouseout(function () {
+        $('li').css('color', '#f9bd04');
+    });
 
 });
